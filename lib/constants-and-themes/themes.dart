df170dc3c01c04a1bddd48099dca6cfef8f5ecec
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-12 04:34:22 +0000

    custom fonts theme for app

--- lib/constants-and-themes/themes.dart
+++ lib/constants-and-themes/themes.dart
@@ -1,7 +1,63 @@
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
+  static TextTheme lightTextTheme = TextTheme(
+    bodyText1: GoogleFonts.poppins(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
+    headline1: GoogleFonts.poppins(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+    headline2: GoogleFonts.poppins(
+      fontSize: 21.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.black,
+    ),
+    headline3: GoogleFonts.poppins(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+    headline6: GoogleFonts.poppins(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.black,
+    ),
+  );
+
+  static TextTheme darkTextTheme = TextTheme(
+    bodyText1: GoogleFonts.poppins(
+      fontSize: 14.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.white,
+    ),
+    headline1: GoogleFonts.poppins(
+      fontSize: 32.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    headline2: GoogleFonts.poppins(
+      fontSize: 21.0,
+      fontWeight: FontWeight.w700,
+      color: Colors.white,
+    ),
+    headline3: GoogleFonts.poppins(
+      fontSize: 16.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    ),
+    headline6: GoogleFonts.poppins(
+      fontSize: 20.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.white,
+    ),
+  );
   static const bgColor = Color(0xFF315ad6);
   static final ThemeData themeData = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: bgColor,
