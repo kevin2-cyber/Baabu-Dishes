3257604cdc0791aab34ae8f2d0cbdae9179e2fa1
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-12 04:57:30 +0000

    update color

--- lib/constants-and-themes/constants.dart
+++ lib/constants-and-themes/constants.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class AppConstants {
   static const title = 'Baabu-Dishes';
   static const bgColor = Color(0xFF315ad6);
-  static const darkThemeTextColor = Color(0xFFf2f2f7);
+  static const darkThemeTextColor = Color(0xFFbabdf2);
 }
