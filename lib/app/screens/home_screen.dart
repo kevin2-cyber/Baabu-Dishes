512459e3642f76ebfa952744f8c3f48ce5b0dc85
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-17 23:32:46 +0000

    minor

--- lib/app/screens/home_screen.dart
+++ lib/app/screens/home_screen.dart
@@ -7,10 +7,6 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-      ),
       body: Container(),
     );
   }
