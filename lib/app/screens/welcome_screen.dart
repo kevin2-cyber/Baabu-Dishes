e6bea189bf1f2b19c3412347b403356fd0bdab83
kevin2-cyber <keduful@st.ug.edu.gh>
2022-04-18 13:52:06 +0000

    push

--- lib/app/screens/welcome_screen.dart
+++ lib/app/screens/welcome_screen.dart
@@ -31,4 +31,24 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
       ),
     );
   }
+
+  Widget backgroundImage() {
+    return ShaderMask(
+      shaderCallback: (bounds) => const LinearGradient(
+        colors: [Colors.black, Colors.black12],
+        begin: Alignment.bottomCenter,
+        end: Alignment.center,
+      ).createShader(bounds),
+      blendMode: BlendMode.darken,
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/sandwich.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken),
+          ),
+        ),
+      ),
+    );
+  }
 }
